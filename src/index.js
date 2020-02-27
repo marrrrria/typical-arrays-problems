c4f7e1a61b16dccae8490b019604092b52c29ca2
Masha <valzyr48@gmail.com>
2020-02-28 02:42:29 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
+exports.min = function min(array) {
+    if (array instanceof Array) {
+        if (array == "") {
+            return 0;
+        }
+        array.sort((a, b) => {
+            return a - b;
+        });
+        return array[0];
+    }
+    return 0;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array instanceof Array) {
+        if (array == "") {
+            return 0;
+        }
+        array.sort((a, b) => {
+            return a - b;
+        });
+        return array[array.length - 1];
+    }
+    return 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array instanceof Array) {
+        if (array == "") {
+            return 0;
+        }
+        let response =
+            array.reduce((a, b) => {
+                return a + b;
+            }) / array.length;
 
-exports.avg = function avg (array) {
-  return 0;
-}
+        return response;
+    }
+    return 0;
+};
